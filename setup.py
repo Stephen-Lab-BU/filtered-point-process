79cf8f8e1c8d9ed1ef364cb770faeaf141ecb85b
Patrick F. Bloniasz <76849809+bloniaszp@users.noreply.github.com>
2024-06-07 20:15:49 -0400

    Setup, filter, and figure changes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    version='0.0.1',
-    url='https://github.com/Stephen-Lab-BU/filtered-point-process.git',
-    license='BSD 3-Clause Clear License',
-    author='Patrick F. Bloniasz',
-    description='',
+    version="0.0.1",
+    url="https://github.com/Stephen-Lab-BU/filtered-point-process.git",
+    license="BSD 3-Clause Clear License",
+    author="Patrick F. Bloniasz",
+    description="",
 )
